7e351666df2ecff70e65432ee853a33f5559520a
shanliu <shan.liu@msn.com>
2019-03-10 14:22:58 +0800

    .

--- tools/tpls/EntityTpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*__LSYS_TPL_P_NAMESPACE__*/
-class __LSYS_TPL_ENTITY__ extends \LSYS\Model\Entity{
-    use \LSYS\Model\Traits\EntityRelatedMethods;
-    use \__LSYS_TPL_AUTO_ENTITY__;
-}
-
-
--- tools/tpls/TraitEntityTpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*__LSYS_TPL_P_AUTO_NAMESPACE__*/
-/*__LSYS_TPL_DOC__*/
-trait __LSYS_TPL_AUTO_ENTITY__{
-    public function tableClass()
-    {
-        RETURN /*__LSYS_TPL_ENTITY_CLASS__*/;
-    }
-}
