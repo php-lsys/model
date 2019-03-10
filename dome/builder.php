08351840f53ee334b0ff0c1765957e4db2745451
shanliu <shan.liu@msn.com>
2019-03-10 15:30:20 +0800

    .

--- dome/builder.php
+++ dome/builder.php
@@ -11,11 +11,14 @@ $entity=$orm->wherePk(1)->find();
 print_r($entity->orm1()->asarray());
 print_r($entity->orm2()->asarray());
 print_r($entity->orm3()->findall()->asarray());
-print_r($entity->orm4()->findall()->asarray());
+$t=$entity->orm4();
+print_r($t->findall()->asarray());
+print_r($t->countall());
 print_r($entity->asArray());
 $orm->db()->foundRows();
 print_r($orm->reset()->where("id", ">", 30)->findAll()->asArray());
 print_r($orm->countAll());
 
 
-//$tm=new \LSYS\Model\Table("user");
+$tm=new \LSYS\Model\Table("address");
+var_dump($tm->where("id", "=", 10)->find()->asArray());
