5c1d6d1334e6e37bbc0f065183d55d6c005afa97
shanliu <shan.liu@msn.com>
2019-03-11 10:26:46 +0800

    ..

--- src/classes/LSYS/Model/Traits/ModelTableColumnsFromDB.php
+++ src/classes/LSYS/Model/Traits/ModelTableColumnsFromDB.php
@@ -8,7 +8,9 @@ trait ModelTableColumnsFromDB{
 	private function _tableColumns(){
 	    $table_name=$this->tableName();
 	    if (!isset(self::$_table_columns_cache[$table_name])){
-	        self::$_table_columns_cache[$table_name]=$this->db()->listColumns($table_name);
+	        $db=$this->db();
+	        assert($db instanceof \LSYS\Model\Database);
+	        self::$_table_columns_cache[$table_name]=$db->listColumns($db->quoteTable($table_name));
 	    }
 	    return self::$_table_columns_cache[$table_name];
 	}
