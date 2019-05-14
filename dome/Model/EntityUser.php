3ff9fd252ed7077a83f27243e2ccfc7fbe4d5acd
shanliu <shanliu@msn.com>
2019-05-14 12:05:36 +0800

    .

--- dome/Model/EntityUser.php
+++ dome/Model/EntityUser.php
@@ -12,4 +12,27 @@ use LSYS\Model\Entity;
 class EntityUser extends Entity{
     use EntityUserTrait;
     use EntityRelatedMethods;
+    //自动校验 自动过滤数据 实现 示例如下
+//     public function filterFactory() {
+//         return (new \LSYS\Entity\Filter($this))->rules(array (
+//             'nickname' => array (
+//                 new \LSYS\Entity\FilterRule\Callback("trim"),
+//                 new \LSYS\Entity\FilterRule\Callback("strip_tags"),
+//             ),
+//             'headimg' => array (
+//                 new \LSYS\Entity\ValidRule\Callback(function($val){
+//                     if(empty($val))return NULL;
+//                     else $val;
+//                 })
+//                 ),
+//                 ));
+//     }
+//     public function validationFactory() {
+//         return (new \LSYS\Entity\Validation($this))->rules(array (
+//             'nickname' => array (
+//                 new \LSYS\Model\ValidRule\StrlenRange(1, 15, 0),
+//             ),
+//         ));
+//     }
+    
 }
--- src/classes/LSYS/Model/Traits/ModelTableColumnsFromFactory.php
+++ src/classes/LSYS/Model/Traits/ModelTableColumnsFromFactory.php
@@ -3,7 +3,11 @@ namespace LSYS\Model\Traits;
 use LSYS\Entity\ColumnSet;
 trait ModelTableColumnsFromFactory{
     private static $_table_columns_code;
-    public function tableColumnsFactory(){
+    /**
+     * 自定义表字段列表,请重写此方法
+     * @return \LSYS\Entity\ColumnSet
+     */
+    private function tableColumnsFactory(){
         return new ColumnSet([]);
     }
     public function tableColumns(){
